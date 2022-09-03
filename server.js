219dceff435096f6e7d9cddc1af3557b89c0c685
Patrick <patasax22@yahoo.com>
2022-09-03 12:00:44 -0400

    mongodb connection

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+require("dotenv").config();
+const multer = require("multer");
+const mongoose = require("mongoose");
+
 const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs')
+const upload = multer({ dest: "uploads" });
+
+mongoose.connect(process.env.DATABASE_URL);
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
 	res.render("index");
 });
 
-app.listen(3000);
+app.post("/upload", upload.single("file"), (req, res) => {
+	res.send("hi");
+});
+
+app.listen(process.env.PORT);
